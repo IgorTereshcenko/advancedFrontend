621ec690a8474a1610ba98b44d5a7af064cb2039
Терещенко Игорь Александрович <igor.tereschenko@vsmd.local>
2023-05-30 08:35:23 +0300

    fix plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,9 +5,10 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+
+    const plugins = [
         new HTMLWebpackPlugin({ // упрощает создание файлов HTML и может автоматически вставлять модули JavaScript в наш основной шаблон HTML
-            template: paths.html // передаем путь
+        template: paths.html // передаем путь
         }),
         new webpack.ProgressPlugin(), // сообщает о прогрессе компиляции сборки
         new MiniCssExtractPlugin({ // используется чтобы css стили билдились отдельными чанками от js кода
@@ -16,9 +17,15 @@ export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPl
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev) // позволяет использовать переменные из конфига глобально во всем приложении
-        }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        }) // удобный анализ бандла
+        })
     ]
+
+    if(isDev) {
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        })) // удобный анализ бандла)
+    }
+
+    return plugins;
+
 }
